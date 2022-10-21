d57fd3be013f652f98f117bea09e5b370b74b29c
kata <kasinkoji53@gmail.com>
2022-10-21 23:12:38 +0700

    ganti background

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div >
-      <nav class="navbar navbar-expand-lg bg-light">
+      <nav class="navbar navbar-expand-lg navbarku">
         <div class="container-fluid">
           <a class="navbar-brand" href="#"> Welcome</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
